b104029d3d6175ef47a2e17c6f8cf933570c4c7a
Christina Ngo <cngo@umd.edu>
2023-11-08 01:02:27 -0500

    changes to weight page

--- Pages/weightTrackProgram.js
+++ Pages/weightTrackProgram.js
@@ -1,32 +1,3 @@
-const exerDict = document.getElementById("prTable");
-
-function newExercise() {
-    var inputExer = document.getElementsById("exer").value; //grab exercise
-    var inputWeight = document.getElementById("weighttrack").value; //grab weight
-
-    if(inputExer === '' || inputWeight === '') {
-        alert("One of your inputs are not filled!");
-    } else {
-        const exercise = {
-            name: inputExer,
-            weights: [inputWeight]
-        }
-
-        exerDict.push(exercise);
-        updateExerciseTable(exerDict, exercise);
-    }  
-}
-
-function updateExerciseTable(table, exercise) {
-    const newRow = table.insertRow(-1);
-
-    const nameCell = newRow.insertCell(0);
-    const weightCell = newRow.insertCell(1);
-
-    nameCell.textContent = exercise.name;
-    weightCell.textContent = exercise.weights[exercise.weights.length-1]; //show most up to date number
-}
-
 $ = function(id) {
     return document.getElementById(id);
 }
@@ -37,4 +8,66 @@ var show = function(id) {
 
 var hide = function(id) {
     $(id).style.display ='none';
+}
+
+function clear(id) {
+    if (id.value != null) {
+        id.value = "";
+    }
+}
+
+const exerciseLst = [];
+
+function newExercise() {
+    var inputExer = document.getElementById("exer").value; //grab exercise
+    var inputWeight = parseFloat(document.getElementById("weighttrack").value); //grab weight
+
+    if(inputExer === '' || isNaN(inputWeight)) {
+        alert("One of your inputs are not filled!");
+    } else {
+
+        const exercise = {
+            name: inputExer,
+            weights: [inputWeight]
+        }
+
+        const exerDict = document.getElementById("prTable");
+
+        exerciseLst.push(exercise);
+        updateExerciseTable(exerDict, exercise, exerciseLst);
+    }  
+
+    clear("exer");
+    clear("weighttrack");
+}
+
+function updateExerciseTable(table, exercise, lst) {
+    const newRow = table.insertRow(-1);
+
+    const nameCell = newRow.insertCell(0);
+    const weightCell = newRow.insertCell(1);
+    const buttonCell = newRow.insertCell(2);
+
+    nameCell.textContent = exercise.name;
+    weightCell.textContent = exercise.weights[exercise.weights.length-1]; //show most up to date number
+
+    const addButton = document.createElement("button");
+    addButton.textContent = "Add Weight";
+    addButton.onclick = () => show('weightPop');
+
+    const historyButton = document.createElement("button");
+    historyButton.textContent = "Check History";
+    historyButton.onclick = () => addWeight(table, exercise, lst, newRow);
+
+    buttonCell.appendChild(addButton);
+    buttonCell.appendChild(historyButton);
+}
+
+function addWeight(table, exercise, lst, row) {
+    const nameCell = row.cells[0];
+    const weightCell = row.cells[1];
+
+    const newWeight = document.createElement("input");
+    newWeight.type = "number";
+    exercise.weights.push(newWeight.value);
 }
