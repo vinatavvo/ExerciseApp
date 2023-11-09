dfbf8f565b17e6f196d26de6abf3be47506cf4b5
Christina Ngo <cngo@umd.edu>
2023-11-08 19:11:24 -0500

    functionality works slay

--- Pages/weightTrackProgram.js
+++ Pages/weightTrackProgram.js
@@ -10,6 +10,7 @@ var hide = function(id) {
     $(id).style.display ='none';
 }
 
+
 function clear(id) {
     if (id.value != null) {
         id.value = "";
@@ -21,6 +22,7 @@ const exerciseLst = [];
 function newExercise() {
     var inputExer = document.getElementById("exer").value; //grab exercise
     var inputWeight = parseFloat(document.getElementById("weighttrack").value); //grab weight
+    var inputDate = document.getElementById("date").value;
 
     if(inputExer === '' || isNaN(inputWeight)) {
         alert("One of your inputs are not filled!");
@@ -28,7 +30,8 @@ function newExercise() {
 
         const exercise = {
             name: inputExer,
-            weights: [inputWeight]
+            weights: [inputWeight],
+            dates: [inputDate]
         }
 
         const exerDict = document.getElementById("prTable");
@@ -37,37 +40,70 @@ function newExercise() {
         updateExerciseTable(exerDict, exercise, exerciseLst);
     }  
 
-    clear("exer");
-    clear("weighttrack");
+    document.getElementById("exer").value = "";
+    document.getElementById("weighttrack").value = "";
 }
 
 function updateExerciseTable(table, exercise, lst) {
     const newRow = table.insertRow(-1);
 
     const nameCell = newRow.insertCell(0);
-    const weightCell = newRow.insertCell(1);
-    const buttonCell = newRow.insertCell(2);
+    const dateCell = newRow.insertCell(1)
+    const weightCell = newRow.insertCell(2);
+    const buttonCell = newRow.insertCell(3);
 
     nameCell.textContent = exercise.name;
+    dateCell.textContent = exercise.dates[exercise.dates.length-1]
     weightCell.textContent = exercise.weights[exercise.weights.length-1]; //show most up to date number
 
     const addButton = document.createElement("button");
     addButton.textContent = "Add Weight";
-    addButton.onclick = () => show('weightPop');
+    addButton.onclick = () => addWeight(newRow);
 
     const historyButton = document.createElement("button");
     historyButton.textContent = "Check History";
-    historyButton.onclick = () => addWeight(table, exercise, lst, newRow);
+    historyButton.onclick = () => giveHistory(newRow);
 
     buttonCell.appendChild(addButton);
     buttonCell.appendChild(historyButton);
 }
 
-function addWeight(table, exercise, lst, row) {
-    const nameCell = row.cells[0];
-    const weightCell = row.cells[1];
+function giveHistory(row) {
 
-    const newWeight = document.createElement("input");
-    newWeight.type = "number";
-    exercise.weights.push(newWeight.value);
+    const exercise = row.cells[0].innerText;
+    const table = document.getElementById("progress").querySelector("tbody");
+    table.innerHTML = "";
+
+    for (const entry of exerciseLst) {
+        if (entry.name === exercise) {
+            for(let i = 0; i < entry.weights.length; i++) {
+                const tableRow = table.insertRow();
+                tableRow.insertCell(0).textContent = entry.dates[i];
+                tableRow.insertCell(1).textContent = entry.weights[i];
+            }
+        }
+    }
+
+    show('histPop');
+
+}
+
+function addWeight(row) {
+    //var newWeight = document.getElementById("pr").value;
+    let exercise = row.cells[0].innerText;
+    let date = row.cells[1];
+    let weight = row.cells[2];
+
+    let weightInput = prompt("Enter your new weight:", weight.textContent);
+    let dateInput = prompt("Enter the date you hit this:", date.textContent);
+
+    for(let i = 0; i < exerciseLst.length; i++) {
+        if(exerciseLst[i].name === exercise) {
+            exerciseLst[i].weights.push(parseFloat(weightInput));
+            exerciseLst[i].dates.push(dateInput);
+        }
+    }
+
+    weight.textContent = weightInput;
+    date.textContent = dateInput;
 }
